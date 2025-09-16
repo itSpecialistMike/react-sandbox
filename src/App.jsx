eae434d13a26df1ff19383c66332535d2fcd780f
itSpecialistMike <mvasilenko401@gmail.com>
2025-09-16 12:00:27 +0700

    Добавил анимации переключения между боксами на главной странице

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
 import './App.css'
 import ToDo from "./features/to-do/ToDo.jsx";
-import {BrowserRouter, Link, Route, Routes, useParams} from 'react-router'
+import {BrowserRouter, Link, Route, Routes} from 'react-router'
 import {NotFound} from "./components/NotFound.jsx";
+import {useState} from "react";
 
 function App() {
+    const [selectedBox, setSelected] = useState('');
+
+    const handleNavLink = (text) => {
+        setSelected(text);
+    }
+
     return (
         <BrowserRouter>
             <header
                 className="flex justify-center items-center bg-primary dark:bg-dark-primary text-base-text dark:secondary-text">
                 <div
                     className="w-full h-50 flex flex-col md:flex-row justify-center items-center gap-4 lg:gap-40 shadow-lg">
-                    <Link to="/todo">Список задач</Link>
-                    <Link to="/calculator">Калькулятор</Link>
-                    <Link to="/galery">Галерея</Link>
-                    <Link to="/ui-sandbox">UI песочница</Link>
+                    <Link to="/todo"
+                          onClick={(e) => handleNavLink(e.target.textContent)}
+                          className={`transition-all duration-300 ease-in-out transform 
+                          ${selectedBox === 'Список задач' ? 'text-3xl': ''}`}>
+                        Список задач
+                    </Link>
+                    <Link to="/calculator"
+                          onClick={(e) => handleNavLink(e.target.textContent)}
+                          className={`transition-all duration-300 ease-in-out transform 
+                          ${selectedBox === 'Калькулятор' ? 'text-3xl': ''}`}>
+                        Калькулятор
+                    </Link>
+                    <Link to="/galery"
+                          onClick={(e) => handleNavLink(e.target.textContent)}
+                          className={`transition-all duration-300 ease-in-out transform 
+                          ${selectedBox === 'Галерея' ? 'text-3xl': ''}`}>
+                        Галерея
+                    </Link>
+                    <Link to="/ui-sandbox"
+                          onClick={(e) => handleNavLink(e.target.textContent)}
+                          className={`transition-all duration-300 ease-in-out transform 
+                          ${selectedBox === 'UI песочница' ? 'text-3xl': ''}`}>
+                        UI песочница
+                    </Link>
                 </div>
             </header>
 
             <div className="flex justify-center items-center gap-5 flex-col mt-5">
-                <h1 className='text-2xl text-dark-primary'>Sandbox </h1>
+                <h1 className='text-2xl text-dark-primary'>Sandbox {selectedBox}</h1>
                 <Routes>
                     <Route path="/todo" element={<ToDo/>}/>
-                    <Route path="*" element={<NotFound />}/>
+                    <Route path="*" element={<NotFound/>}/>
                 </Routes>
             </div>
 
